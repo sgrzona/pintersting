fa810795cf2c5b51727c64fcffb398c5f08e1244
Stephanie Grzona <stephanie@800ceoread.com>
2014-11-03 12:53:05 -0600

    Added Work Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/work'
+
   root "pages#home"
   get "about" => "pages#about"
+  get "work" => "pages#work"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
